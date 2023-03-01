06500c77423b540e5a247d3f1f7e715c3d8709f0
Joshua Juanchini <joshua.juanchini@motorolasolutions.com>
2023-03-01 12:32:12 -0300

    use axios to get account

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios'
 import './App.css';
 
 function App() {
-  function testGetContacts(){
-    alert('Testing');
+  async function testGetContacts(){
+    console.log('Starting...');
+
+    const response = await axios.get('https://fctgcorporate--dataart2.sandbox.lightning.force.com/services/data/v57.0/sobjects/Account/0013M00001HsuUKQAZ');
+    console.log('response  ', response);
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={testGetContacts}>Click To Get Contacts</button>
+        <Button onClick={testGetContacts}>Click To Get Contacts</Button>
       </header>
     </div>
   );
